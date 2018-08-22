444a12fe758118bb1db8b9c8ccf255897b97302d
keith <kdang@quartermaster.house>
2018-08-22 11:45:20 -0400

    set proxy

--- services/passport.js
+++ services/passport.js
@@ -16,7 +16,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     // (accessToken, refreshToken, profile, done) => {
     //   new User({ googleId: profile.id }).save();
