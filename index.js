8134b1c1df6c784dbcc834a1db454efe4c208cc0
keith <kdang@quartermaster.house>
2018-08-22 20:15:34 -0400

    production

--- index.js
+++ index.js
@@ -21,8 +21,18 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+
 require("./routes/authRoutes")(app);
 require("./routes/pointRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  //Express will serve up production assets like out main.js file or main.css file
+  app.use(express.static("client/build"));
+  //Express will serve up the index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
