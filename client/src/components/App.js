ba3556317c3a37752b164544b87826472dc8a8d0
keith <kdang@quartermaster.house>
2018-08-24 19:52:43 -0400

    Reverted to 4b66546ff3075bfdc4b5de57d03d0c651536af28

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import "../App.less";
 import Header from "./Header";
 import Landing from "./Landing";
 import { connect } from "react-redux";
